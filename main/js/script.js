ada1a35f48eab7822a4baa63f03d9b625e563e92
shpilson <kovalenko.jora@gmail.com>
2023-01-24 00:31:40 -0300

    fixes

--- main/js/script.js
+++ main/js/script.js
@@ -1,3 +1,39 @@
+//preloader
+window.onload = function () {
+  let preloader = document.querySelector(".preloader");
+  preloader.style.display = "none";
+};
+
+//lazy load
+const lazyLoadImage = () => {
+  const images = document.querySelectorAll("img");
+  const optionsLazyLoad = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  function handleImg(myImg, observerLazyLoad) {
+    myImg.forEach((myImgSingle) => {
+      // console.log(myImgSingle.intersectionRatio);
+      if (myImgSingle.intersectionRatio > 0) {
+        loadImage(myImgSingle.target);
+      }
+    });
+  }
+
+  function loadImage(image) {
+    image.src = image.getAttribute("data");
+  }
+
+  const observerLazyLoad = new IntersectionObserver(handleImg, optionsLazyLoad);
+
+  images.forEach((img) => {
+    observerLazyLoad.observe(img);
+  });
+};
+lazyLoadImage();
+
 const slider = () => {
   $(document).ready(function () {
     $(".slider").slick({
